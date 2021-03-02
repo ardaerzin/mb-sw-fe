3a568048d32be2b6dd23e7af500315d54cb46254
Arda Erzin <ardaerzin@gmail.com>
2021-03-02 15:47:51 +0100

    added framer-motion
    - init with lazy config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { MotionConfig } from 'framer-motion'
+import { useEffect, useState } from 'react'
 import 'Styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [features, setFeatures] = useState([])
+  useEffect(() => {
+    import('lib/motionConfig').then(res => {
+      setFeatures(res.default)
+    })
+  }, [])
+  return (
+    <MotionConfig features={features}>
+      <Component {...pageProps} />
+    </MotionConfig>
+  )
 }
 
 export default MyApp
