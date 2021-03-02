4c7e23c16bc6c8b8aeab41ccc0ece0db26c3b9ac
Arda Erzin <ardaerzin@gmail.com>
2021-03-02 20:08:54 +0100

    added missing tw aspect ratio plugin config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}'],
   plugins: [
+    require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/forms')
   ]
