0ef96589e7eedbb0744e849335ebf5ebcc0fd4fe
Arda Erzin <ardaerzin@gmail.com>
2021-03-03 10:15:23 +0100

    search functionality for index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import { BsFilterRight } from 'react-icons/bs'
+import PeopleItem from 'Components/Thumbnails/Person'
 import { AllPeople } from 'lib/graph/Queries'
 import request from 'lib/graph/Utils/request'
 import Cookies from 'universal-cookie'
-import PeopleItem from 'Components/Thumbnails/Person'
 import { NextSeo } from 'next-seo'
+import { AnimateSharedLayout } from 'framer-motion'
+import useSearch from 'lib/hooks/useSearch'
 
-const Home = ({ people }) => {
+const Home = (props) => {
+  const { people, loading } = useSearch(props.people)
   return (
     <div
       className='
@@ -27,30 +29,48 @@ const Home = ({ people }) => {
           site_name: 'StarWars Dex',
         }}
       />
-      <div
-        className='
-          w-full
-          grid grid-cols-2 sm:lg:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4
-          max-w-6xl
-          p-4
-        '
-      >
-        {people.map((pi) => (
-          <PeopleItem
-            key={`sw-people-item-${pi.id}`}
-            {...pi}
-          />
-        ))}
-      </div>
+      {
+        people.length > 0 ? (
+          <>
+            <span className='text-3xl self-start mx-auto md:mx-0'>
+              {people.length} characters found
+            </span>
+            <AnimateSharedLayout>
+              <div
+                className='
+                  w-full
+                  grid grid-cols-2 sm:lg:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4
+                  max-w-6xl
+                  p-4
+                '
+              >
+                {people.map((pi) => (
+                  <PeopleItem
+                    key={`sw-people-item-${pi.id}`}
+                    {...pi}
+                  />
+                ))}
+              </div>  
+            </AnimateSharedLayout>
+          </>
+        ) : (
+          <span>
+            no characters were found. please try with a different name
+          </span>
+        )
+      }
     </div>
   )
 }
 
 export async function getServerSideProps({ query, req, res, ...rest }) {
+
+  /**
+   * redirect user to login if no tokens are found
+   */
   var header = req && req.headers && req.headers.cookie
   var uc = new Cookies(header)
   const { userToken } = uc.getAll()
-
   if (!userToken) {
     res.writeHead(302, { Location: '/auth' });
     res.end()
