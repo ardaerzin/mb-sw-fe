ee226631e699eaf988a319d0231feef59e884a60
Arda Erzin <ardaerzin@gmail.com>
2021-03-03 10:13:44 +0100

    header search component
    - on index page, index list is updated without search views
    - on other pages, the search component is displayed
    
    - also added layout animation config for framer-motion

--- Components/Header.js
+++ Components/Header.js
@@ -1,44 +1,104 @@
-import { FaRebel } from 'react-icons/fa'
+import { FaEmpire, FaRebel } from 'react-icons/fa'
+import { CgFormatSlash } from 'react-icons/cg'
+import useSharedState from 'lib/hooks/useSharedState'
+import { useRouter } from 'next/router'
+import SearchResults from 'Components/Search'
+import { AnimatePresence } from 'framer-motion'
+import Link from 'next/link'
+import { AiFillCloseCircle } from 'react-icons/ai'
 
 const Header = props => {
+  const [search, setSearch] = useSharedState('searchState', { text: undefined, status: false })
+  const router = useRouter()
+
+  /**
+   * fire with changing search query
+   * @param {*} event fired
+   */
+  const handleSearchChange = (e) => {
+    setSearch({ text: e.target.value, status: e.target.value !== '' })
+  }
+
+  /**
+   * reset search state
+   */
+  const resetSearch = () => {
+    setSearch({ text: undefined, state: false })
+  }
+
   return (
-    <header
-      className='
-        sticky
-        top-0
-        z-50
-        w-full
-        py-2 px-2 md:px-8 xl:px-0
-        bg-white
-        shadow-lg
-      '
-    >
-      <div
-        className='mx-auto max-w-screen-xl w-full flex justify-between'
+    <>
+      <header
+        className='
+          fixed
+          left-0
+          top-0
+          z-50
+          w-full
+          py-2 px-2 md:px-8 xl:px-0
+          bg-white
+          shadow-lg
+        '
       >
-        <div className='flex items-center space-x-1'>
-          <div className='flex items-center justify-center'>
-            <FaRebel
-              className='text-2xl md:text-lg text-purple-500'
-            />
+        <div
+          className='mx-auto max-w-screen-xl w-full flex justify-between'
+        >
+          <div className='flex items-center space-x-1'>
+            <Link
+              href='/'
+            >
+              <a className='flex items-center justify-center'>
+                <FaRebel
+                  className='text-2xl md:text-lg text-yellow-600'
+                />
+                <CgFormatSlash
+                  className='text-2xl md:text-lg text-gray-500'
+                />
+                <FaEmpire
+                  className='text-2xl md:text-lg text-gray-700'
+                />
+              </a>
+            </Link>
+            <span className='hidden md:block font-semibold text-lg text-yellow-600 pl-1'>
+              StarWars Dex
+            </span>
           </div>
-          <span className='hidden md:block font-semibold text-lg'>
-            sw: legends
-          </span>
+          <div className='relative'>
+            <input
+              type='text'
+              placeholder='search by name'
+              value={search.text || ''}
+              onChange={handleSearchChange}
+              className='
+                border-b
+                text-sm
+                rounded-lg
+                bg-transparent
+                focus:outline-none focus:border-brand focus:ring-0
+              '
+            />
+            <div
+              className='absolute right-0 top-0 h-full flex items-center pr-2 cursor-pointer'
+            >
+              <AiFillCloseCircle
+                onClick={resetSearch}
+                className='text-red-500 text-lg'
+              />
+            </div>
+          </div>
         </div>
-        <input
-          type='text'
-          placeholder='search here'
-          className='
-            border-b
-            text-sm
-            rounded-lg
-            bg-transparent
-            focus:outline-none focus:border-brand focus:ring-0
-          '
-        />
-      </div>
-    </header>
+      </header>
+      <AnimatePresence>
+        {
+          /**
+           * do no show search results if we are on the index page
+           */
+          (router.pathname !== '/' && search.status) && (
+            <SearchResults />
+          )
+        }
+      </AnimatePresence>
+    </>
   )
 }
 
---
+++ Components/Search.js
@@ -0,0 +1,101 @@
+import useSharedState from 'lib/hooks/useSharedState'
+import { AnimateSharedLayout, m as motion } from 'framer-motion'
+import PropTypes from 'prop-types'
+import useGraphSWR from 'lib/graph/Utils/useGraphSWR'
+import useSWR from 'swr'
+import { AllPeople } from 'lib/graph/Queries'
+import useSearch from 'lib/hooks/useSearch'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const SearchResultItem = (props) => {
+  return (
+    <Link
+      href={`/character/${props.id}`}
+    >
+      <motion.a
+        layout
+        layoutId={`search-item-${props.id}`}
+        className='w-full flex space-x-1 items-center bg-white shadow-md rounded-md cursor-pointer'
+      >
+        <div className='w-12 h-12 rounded-small shadow-md relative overflow-hidden'>
+          <Image
+            src={props.image}
+            layout='fill'
+          />
+        </div>
+        <span className='text-lg'>
+          {props.name}
+        </span>
+      </motion.a>
+    </Link>
+  )
+}
+
+const SearchResults = ({ text }) => {
+  const { people = [], loading } = useSearch()
+  console.log('what are people?', people)
+  return (
+    <motion.div
+      className='
+        fixed left-0 top-14
+        flex
+        justify-center
+        w-full
+        pb-2
+        z-20
+      '
+      exit='exit'
+      animate='enter'
+      initial='initial'
+      variants={{
+        exit: { height: '0' },
+        initial: { height: '0' },
+        enter: { height: '300px' }
+      }}
+      transition={{
+        type: 'spring',
+        damping: 40,
+        stiffness: 400
+      }}
+    >
+      <motion.div
+        className='
+          flex flex-col
+          overflow-y-scroll
+          items-end
+          max-w-screen-xl
+          mx-auto
+          mr-2 md:mr-8 lg:mr-auto
+          w-full
+        '
+      >
+        <AnimateSharedLayout>
+          <div
+            className='
+              flex flex-col
+              overflow-y-scroll
+              space-y-2
+              w-max
+            '
+          >
+            {
+              people.map((pi) => (
+                <SearchResultItem
+                  key={`search-result-${pi.id}`}
+                  {...pi}
+                />
+              ))
+            }
+          </div>
+        </AnimateSharedLayout>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+SearchResults.propTypes = {
+
+}
+
+export default SearchResults
