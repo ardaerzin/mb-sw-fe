7805a69e430cbca944aac77d79fda748e68c7689
Arda Erzin <ardaerzin@gmail.com>
2021-03-03 18:14:40 +0100

    feedback hunt #3
    - zero state for empty lists in character details page

--- Components/Character/ListSection.js
+++ Components/Character/ListSection.js
@@ -5,20 +5,27 @@ import { cloneElement } from 'react'
 const CharacterItemListSection = ({ children, keyValue, label, character = {}}) => {
   const { name } = character
   const items = character[keyValue] || []
-  // console.log('character?', character, character[keyValue])
   return (
     <CharacterSection className='flex-col space-x-1 space-y-1'>
       <CharacterSectionHeader>
         <span className='text-purple-500'>{items.length}</span> {label}
       </CharacterSectionHeader>
-      <div className='pt-4 grid grid-cols-1 md:grid-cols-3 gap-4'>
-        {items.map((fi, i) => (
-          cloneElement(children, {
-            key: `character-${keyValue}-${fi.id}`,
-            ...fi
-          })
-        ))}
-      </div>
+      {
+        items.length > 0 ? (
+          <div className='pt-4 grid grid-cols-1 md:grid-cols-3 gap-4'>
+            {items.map((fi, i) => (
+              cloneElement(children, {
+                key: `character-${keyValue}-${fi.id}`,
+                ...fi
+              })
+            ))}
+          </div>
+        ) : (
+          <span className='ml-2'>
+            this character does not have any <b>{keyValue}</b> information
+          </span>
+        )
+      }
     </CharacterSection>
   )
 }
